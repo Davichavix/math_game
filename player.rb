1ef02313e18604194dbb4bc2b3ff5b02d0e0cf18
David Chan <dchan902@gmail.com>
2022-04-05 22:02:09 -0600

    create test player

--- player.rb
+++ player.rb
@@ -1,10 +1,13 @@
 class Player
   
-  def initialize(name , lives)
+  def initialize(name , lives = 3)
     @name = name
     @lives = lives
   end
 
-  attr_accessor :name
+  attr_accessor :name, :lives
 
 end
+
+p = Player.new("David", 4)
+puts p.name
